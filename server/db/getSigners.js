22705a3af81afe290acf9c615af325d12449adde
Lorenzo Fontoura <lorenzo@nioel.com>
2021-05-03 14:08:00 +1000

    add getSigners module

---
+++ server/db/getSigners.js
@@ -0,0 +1,20 @@
+﻿const pool = require("./index").pool;
+
+let signersCache = [];
+let lastFetchTime = 0;
+const maxCacheTime = 500;
+
+const updateCache = async () => {
+  const client = await pool.connect();
+  signersCache = (await client.query(`
+    SELECT *
+    FROM signatures
+  `))?.rows?.map(x => x.signature) ?? [];
+  client.release();
+};
+
+module.exports = async () => {
+  if (Date.now() - lastFetchTime > maxCacheTime)
+    await updateCache();
+  return signersCache;
+};
